d24c647853ed04a0e20616f6679f6ee9a9f82c42
Johnson Xin <2775145275@qq.com>
2023-02-03 18:47:38 +0800

    update 添加icon组件

--- packages/wview-components/src/App.vue
+++ packages/wview-components/src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Button from './components/button'
+import IconSystem from './components/icons'
 </script>
 
 <template>
   <div>这里是wview组件库</div>
   <Button class="btn"></Button>
+  <div>Iconfont</div>
+  <!-- <IconSystem name="jiahao" dot /> -->
+  <!-- <IconSystem name="jiahao1" dot badge="1" /> -->
+  <IconSystem name="jianhao" />
+  <IconSystem name="system" />
+  <IconSystem name="system_complete_fill" />
+  <IconSystem name="system_expired_fill" />
 </template>
 
 <style lang="scss">
