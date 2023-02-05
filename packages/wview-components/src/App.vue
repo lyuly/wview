d1b519a51dfe633acc46361274b6731eba9289c1
Johnson Xin <2775145275@qq.com>
2023-02-05 13:16:21 +0800

    fix 解决冲突

--- packages/wview-components/src/App.vue
+++ packages/wview-components/src/App.vue
@@ -5,77 +5,6 @@ import IconSystem from './components/icons'
 
 <template>
   <div>这里是wview组件库</div>
-<<<<<<< HEAD
-
-  <Button class="btn normal"></Button>
-  <Button class="btn warn"></Button>
-  <Button class="btn error"></Button>
-  <!-- TODO：鼠标悬浮时显示禁用原因(可自定义) -->
-  <Button class="btn disabled" disabled></Button>
-</template>
-
-<style lang="scss">
-.btn {
-  text-align: center;
-  border: none;
-  padding: 0.5em 1em;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: bold;
-  border-radius: 6px;
-  cursor: pointer;
-  color: grey;
-  margin-left: 20px;
-  transition: all 0.2s;
-}
-
-.normal {
-  background-color: $white-6;
-  border: 3px solid rgb(183, 112, 183);
-}
-
-.normal:hover {
-  color: $purple;
-  background-color: rgb(225, 170, 225);
-}
-
-.normal:active {
-  border: 3px solid rgb(81, 0, 81);
-}
-
-.warn {
-  background-color: $orange;
-  color: $white;
-}
-
-.warn:hover {
-  background-color: rgb(249, 152, 117);
-}
-
-.warn:active {
-  background-color: rgb(182, 67, 26);
-}
-
-.error {
-  background-color: $red;
-  color: $white;
-}
-
-.error:hover {
-  background-color: #f47882;
-}
-
-.error:active {
-  background-color: #aa0e1b;
-}
-
-.disabled {
-  background-color: $white-6;
-  border: 3px solid rgb(183, 112, 183);
-  cursor: not-allowed;
-}
-</style>
-=======
   <Button type="primary">默 认</Button>
   <div>Iconfont</div>
   <!-- <IconSystem name="jiahao" dot /> -->
@@ -85,4 +14,3 @@ import IconSystem from './components/icons'
   <IconSystem name="system_complete_fill" />
   <IconSystem name="system_expired_fill" />
 </template>
->>>>>>> upstream/main
--- packages/wview-components/src/components/button/index.ts
+++ packages/wview-components/src/components/button/index.ts
@@ -1,10 +1,6 @@
 import type { App } from 'vue'
 
-<<<<<<< HEAD
-import Button from './button'
-=======
 import wButton from './wButton.vue'
->>>>>>> upstream/main
 
 wButton.install = (app: App) => {
   app.component(wButton.name, wButton)
