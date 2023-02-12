0b992b515172430239e51ae24bdae682fb0cb6ec
Johnson Xin <Reday_King@163.com>
2023-02-12 16:02:12 +0800

    update icon-docs (#44)
    * update icon-docs

---
+++ packages/document/docs/src/example/basic/icon/mediaIconList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <table>
+      <tr>
+        <th>
+          <Icon class="icon" name="maikefeng" />
+        </th>
+        <th>
+          <Icon class="icon" name="mic-off" />
+        </th>
+        <th>
+          <Icon class="icon" name="zanting" />
+        </th>
+        <th>
+          <Icon class="icon" name="bofang" />
+        </th>
+        <th>
+          <Icon class="icon" name="erji" />
+        </th>
+        <th>
+          <Icon class="icon" name="pc" />
+        </th>
+        <th>
+          <Icon class="icon" name="fenleisheyingshuma" />
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <Icon class="icon" name="zhaopian" />
+        </th>
+        <th>
+          <Icon class="icon" name="taiji" />
+        </th>
+        <th>
+          <Icon class="icon" name="shouji" />
+        </th>
+        <th>
+          <Icon class="icon" name="sheyingji01" />
+        </th>
+        <th>
+          <Icon class="icon" name="charutupian" />
+        </th>
+        <th>
+          <Icon class="icon" name="bg-pc" />
+        </th>
+        <th>
+          <Icon class="icon" name="zhaoxiangji" />
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <Icon class="icon" name="lingsheng" />
+        </th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from 'wview'
+</script>
+
+<style lang="scss" scoped>
+@import './style/common.scss';
+</style>
