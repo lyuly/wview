4767e32d44e6f3bdc3a780d50cc884ca9eecc859
Johnson Xin <2775145275@qq.com>
2023-02-12 15:54:18 +0800

    update icon-docs

--- packages/document/docs/src/example/basic/icon/arrowIconList.vue
+++ packages/document/docs/src/example/basic/icon/arrowIconList.vue
@@ -1,7 +1,81 @@
 <template>
-  <div class="box">
-    <Icon class="icon" name="system_sort_ascending_line" />
-    <Icon class="icon" name="system_arrow_right_line" />
+  <div class="container">
+    <table>
+      <tr>
+        <th>
+          <Icon class="icon" name="system_arrow_left_line" />
+        </th>
+        <th>
+          <Icon class="icon" name="system_arrow_up_line" />
+        </th>
+        <th>
+          <Icon class="icon" name="system_arrow_right_line" />
+        </th>
+        <th>
+          <Icon class="icon" name="system_arrow_down_line" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-double-right" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-double-left" />
+        </th>
+        <th>
+          <Icon class="icon" name="cc-arrow-up" />
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <Icon class="icon" name="cc-arrow-down" />
+        </th>
+        <th>
+          <Icon class="icon" name="cc-arrow-left" />
+        </th>
+        <th>
+          <Icon class="icon" name="cc-arrow-right" />
+        </th>
+        <th>
+          <Icon class="icon" name="jiantou_youshang_o" />
+        </th>
+        <th>
+          <Icon class="icon" name="jiantou_zuoshang_o" />
+        </th>
+        <th>
+          <Icon class="icon" name="jiantou_zuoxia_o" />
+        </th>
+        <th>
+          <Icon class="icon" name="jiantou_youxia_o" />
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <Icon class="icon" name="system_sort_line" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-up-filling" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-down-filling" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-left-filling" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow-right-filling" />
+        </th>
+        <th>
+          <Icon class="icon" name="arrow_right_arrow_left" />
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <Icon class="icon" name="Arrows" />
+        </th>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -10,12 +84,5 @@ import { Icon } from 'wview'
 </script>
 
 <style lang="scss" scoped>
-.icon {
-  width: 1.5em;
-  height: 1.5em;
-  color: #303133;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
+@import './style/common.scss';
 </style>
