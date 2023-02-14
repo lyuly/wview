f82bc083f83a5040046e379cd11f27cca28d228e
Johnson Xin <Reday_King@163.com>
2023-02-14 22:40:04 +0800

    update icon (#61)
    * update icon docs and component
    
    * update eslintignore

--- packages/document/docs/src/example/basic/icon/weatherIconList.vue
+++ packages/document/docs/src/example/basic/icon/weatherIconList.vue
@@ -3,76 +3,33 @@
     <table>
       <tr>
         <th>
-          <Icon class="icon" name="system_arrow_left_line" />
+          <Icon class="icon" name="weather_dongxue" />
         </th>
         <th>
-          <Icon class="icon" name="system_arrow_up_line" />
+          <Icon class="icon" name="WeatherRain" />
         </th>
         <th>
-          <Icon class="icon" name="system_arrow_right_line" />
+          <Icon class="icon" name="weather2" />
         </th>
         <th>
-          <Icon class="icon" name="system_arrow_down_line" />
+          <Icon class="icon" name="weather_qing" />
         </th>
         <th>
-          <Icon class="icon" name="arrow-double-right" />
+          <Icon class="icon" name="weather_leizhenyu" />
         </th>
         <th>
-          <Icon class="icon" name="arrow-double-left" />
+          <Icon class="icon" name="weather_yin" />
         </th>
         <th>
-          <Icon class="icon" name="cc-arrow-up" />
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <Icon class="icon" name="cc-arrow-down" />
-        </th>
-        <th>
-          <Icon class="icon" name="cc-arrow-left" />
-        </th>
-        <th>
-          <Icon class="icon" name="cc-arrow-right" />
-        </th>
-        <th>
-          <Icon class="icon" name="jiantou_youshang_o" />
-        </th>
-        <th>
-          <Icon class="icon" name="jiantou_zuoshang_o" />
-        </th>
-        <th>
-          <Icon class="icon" name="jiantou_zuoxia_o" />
-        </th>
-        <th>
-          <Icon class="icon" name="jiantou_youxia_o" />
+          <Icon class="icon" name="weather_xiaoyu" />
         </th>
       </tr>
       <tr>
         <th>
-          <Icon class="icon" name="system_sort_line" />
+          <Icon class="icon" name="weather_wu" />
         </th>
         <th>
-          <Icon class="icon" name="arrow" />
-        </th>
-        <th>
-          <Icon class="icon" name="arrow-up-filling" />
-        </th>
-        <th>
-          <Icon class="icon" name="arrow-down-filling" />
-        </th>
-        <th>
-          <Icon class="icon" name="arrow-left-filling" />
-        </th>
-        <th>
-          <Icon class="icon" name="arrow-right-filling" />
-        </th>
-        <th>
-          <Icon class="icon" name="arrow_right_arrow_left" />
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <Icon class="icon" name="Arrows" />
+          <Icon class="icon" name="WeatherMoon" />
         </th>
       </tr>
     </table>
