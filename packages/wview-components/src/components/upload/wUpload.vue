098c177beec9b45948c374ea45153e5c4cd1158f
NorthNoah <1264918006@qq.com>
2023-02-15 18:19:13 +0800

    feat: upload文件列表展示 文件删除功能实现

--- packages/wview-components/src/components/upload/wUpload.vue
+++ packages/wview-components/src/components/upload/wUpload.vue
@@ -1,11 +1,74 @@
+<!-- eslint-disable import/first/export -->
 <template>
   <div class="w-upload">
-    <input type="file" />
+    <input
+      type="file"
+      :multiple="props.multiple"
+      :accept="props.accept"
+      ref="wIpt"
+      @change="getFiles"
+    />
+    <div @click="fileUpload">
+      <slot />
+    </div>
+    <div class="w-upload-list">
+      <div class="w-upload-list_item" v-for="(item, index) in filesList" :key="index">
+        <div class="k-upload-list_item-name">
+          {{ item.name }}
+        </div>
+        <div class="k-upload-list_item-status-label">
+          <Icon name="cuowu" @click="delFile(index)" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<!-- <script>
+export default {
+  name: 'w-upload'
+}
+</script> -->
+
 <script setup lang="ts">
 import '@/styles/components/upload/index.scss'
+import { ref } from 'vue'
+import Icon from '../icons/wIcon.vue'
+
+// 传入参数props
+type UploadType = {
+  multiple?: boolean
+  accept?: string
+  drag?: boolean
+}
+const props = defineProps<UploadType>()
+
+// emits事件
+const emits = defineEmits(['getFilesList'])
+
+// 获取dom元素
+const wIpt = ref()
+
+// 获取文件列表
+const filesList = ref<File[]>([])
+const fileUpload = () => {
+  // 触发input点击事件
+  wIpt.value.click()
+}
+
+const getFiles = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files
+  if (!files) return
+  filesList.value.push(...Array.from(files))
+
+  // 将文件列表传给父组件
+  emits('getFilesList', filesList.value)
+}
+
+const delFile = (index: number) => {
+  filesList.value.splice(index, 1)
+  emits('getFilesList', filesList.value)
+}
 </script>
 
 <style lang="scss" scoped></style>
