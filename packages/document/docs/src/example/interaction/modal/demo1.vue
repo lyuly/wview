b21125cedd4f32ebe580aa69132ac2c4e0e6f2cd
cxk_git <1720921998@qq.com>
2023-02-17 19:41:38 +0800

    update modal

---
+++ packages/document/docs/src/example/interaction/modal/demo1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Button type="primary" @click="state.isShow = true">打开模态框</Button>
+    <Modal v-model:show="state.isShow" title="模态框标题">
+      内容区域
+      <template #footer>
+        <Button type="primary" @click="state.isShow = false">确认</Button>
+        <Button type="default" @click="state.isShow = false">取消</Button>
+      </template>
+    </Modal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button, Modal } from 'wview'
+import { reactive } from 'vue'
+const state = reactive({
+  isShow: false
+})
+</script>
+
+<style scoped></style>
