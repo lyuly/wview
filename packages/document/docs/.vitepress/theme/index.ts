76f979f16c5f1ae8f1807c0e1b8523878c125d1c
lyuly <liumike567@gmail.com>
2023-02-20 14:34:53 +0800

    update remove mp3

--- packages/document/docs/.vitepress/theme/index.ts
+++ packages/document/docs/.vitepress/theme/index.ts
@@ -2,20 +2,9 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import 'wviewui/dist/styles.css'
 import { EnhanceAppContext } from 'vitepress'
-import vitepressMusic from 'vitepress-plugin-music'
-import 'vitepress-plugin-music/lib/css/index.css'
-
-const playlist = [
-  {
-    name: '群青',
-    author: 'YOASOBI',
-    file: '/群青.mp3',
-  }
-]
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx: EnhanceAppContext) {
-    vitepressMusic(playlist)
   }
 }
