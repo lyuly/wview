702758026ebe7cfef3c14210c33069c22b366875
lyuly <20879193+lyuly@users.noreply.github.com>
2023-03-03 11:59:07 +0800

    update alert docs (#100)

---
+++ packages/document/docs/src/example/other/alert/sizeAlert.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Alert } from 'wviewui'
+</script>
+
+<template>
+  <Alert type="success" size="mini">成功警告</Alert>
+  <Alert type="warning" size="small">严重警告</Alert>
+  <Alert type="info">通知警告</Alert>
+  <Alert type="danger" size="large">危险警告</Alert>
+</template>
+
+<style scoped></style>
